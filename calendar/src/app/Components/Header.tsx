31f2bb7e95384b56abfe6c8555bf6d4940026639
Faniry <faniryrandria08@gmail.com>
2024-09-27 11:11:57 +0300

    fix: change route

--- calendar/src/app/Components/Header.tsx
+++ calendar/src/app/Components/Header.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 const HeaderPage = () => {
     const router = useRouter();
     const handleClick = () => {
-        router.push('/Users');
+        router.push('/login');
     }
     return(
         <main>
